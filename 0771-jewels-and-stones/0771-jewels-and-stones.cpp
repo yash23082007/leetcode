93fe06e236f4433f3773b1fac218eedf519f7dc9
Yash vijay <ktanayash@gmail.com>
2026-07-23 16:08:34 +0530

    Time: 0 ms (100%), Space: 8.3 MB (79.75%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -11,7 +11,7 @@ public:
 
                 if (stones[i] == jewels[j]) {
                     count++;
-                    break;
+                    
                 }
             }
         }
