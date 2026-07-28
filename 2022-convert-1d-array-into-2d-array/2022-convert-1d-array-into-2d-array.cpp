5deccd9a1aebcbe870755e79aa3d8e6a7f7fc67c
Yash vijay <ktanayash@gmail.com>
2026-07-28 17:12:27 +0530

    Time: 7 ms (84.47%), Space: 89.9 MB (63.26%) - LeetHub

---
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+
+        if (m * n != original.size())
+            return {};
+
+        vector<vector<int>> ans(m, vector<int>(n));
+
+        int index = 0;
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                ans[i][j] = original[index];
+                index++;
+            }
+        }
+
+        return ans;
+    }
+};
